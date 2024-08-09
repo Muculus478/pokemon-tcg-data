29dae83cfa64ab983ffc8001befff1dce3c8f992
Muculus478 <esevart@gmail.com>
2024-08-09 15:03:55 -0400

    Mock Data

--- scripts/OpenURL.ps1
+++ scripts/OpenURL.ps1
@@ -1,53 +1,151 @@
-# Specify the URL you want to open
-#$URL = "https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660"
-#curl $URL
-#Add-Type -AssemblyName System.Windows.Forms
-#Add-Type -AssemblyName System.Drawing
-
-# Create the form
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Select a URL"
-$form.Size = New-Object System.Drawing.Size(400, 250)
-$form.StartPosition = "CenterScreen"
-
-# Create radio buttons
-$radioButton1 = New-Object System.Windows.Forms.RadioButton
-$radioButton1.Location = New-Object System.Drawing.Point(20, 30)
-$radioButton1.Size = New-Object System.Drawing.Size(350, 20)
-$radioButton1.Text = "Muddy Waters - SGLC Semi Gym Leader Challenge"
-$radioButton1.Tag = "https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660"
-$form.Controls.Add($radioButton1)
-
-$radioButton2 = New-Object System.Windows.Forms.RadioButton
-$radioButton2.Location = New-Object System.Drawing.Point(20, 60)
-$radioButton2.Size = New-Object System.Drawing.Size(350, 20)
-$radioButton2.Text = "Dark Cycle - SGLC Semi Gym Leader Challenge"
-$radioButton2.Tag = "https://pokemoncard.io/deck/dark-cycle-sglc-semi-gym-leader-challenge-35671"
-$form.Controls.Add($radioButton2)
-
-$radioButton3 = New-Object System.Windows.Forms.RadioButton
-$radioButton3.Location = New-Object System.Drawing.Point(20, 90)
-$radioButton3.Size = New-Object System.Drawing.Size(350, 20)
-$radioButton3.Text = "Rainbow Deck - SGLC Semi Gym Leader Challenge"
-$radioButton3.Tag = "https://pokemoncard.io/deck/rainbow-deck-sglc-semi-gym-leader-challenge-35669"
-$form.Controls.Add($radioButton3)
-
-# Create a button
-$button = New-Object System.Windows.Forms.Button
-$button.Location = New-Object System.Drawing.Point(150, 150)
-$button.Size = New-Object System.Drawing.Size(100, 30)
-$button.Text = "Go"
-$form.Controls.Add($button)
-
-# Button click event
-$button.Add_Click({
-    $selectedRadioButton = $form.Controls | Where-Object { $_.Checked -eq $true }
-    if ($selectedRadioButton) {
-        Start-Process $selectedRadioButton.Tag
-    } else {
-        [System.Windows.Forms.MessageBox]::Show("Please select a URL.", "Warning")
-    }
-})
-
-# Show the form
-[void] $form.ShowDialog()
+<div class="" style="--num-cards: 60"><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh2-99">
+<img class="lazy" data-name="swsh2-99" data-card="swsh2-99" data-cardname="Barboach" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh2/swsh2-99.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh7-119">
+<img class="lazy" data-name="swsh7-119" data-card="swsh7-119" data-cardname="Drampa" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh7/swsh7-119.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-56">
+<img class="lazy" data-name="swsh1-56" data-card="swsh1-56" data-cardname="Drizzile" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh1/swsh1-56.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm5-98">
+<img class="lazy" data-name="sm5-98" data-card="sm5-98" data-cardname="Gabite" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm5/sm5-98.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh9-109">
+<img class="lazy" data-name="swsh9-109" data-card="swsh9-109" data-cardname="Garchomp" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh9/swsh9-109.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm5-96">
+<img class="lazy" data-name="sm5-96" data-card="sm5-96" data-cardname="Gible" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm5/sm5-96.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-58">
+<img class="lazy" data-name="swsh1-58" data-card="swsh1-58" data-cardname="Inteleon" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh1/swsh1-58.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh8-63">
+<img class="lazy" data-name="swsh8-63" data-card="swsh8-63" data-cardname="Marshtomp" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh8/swsh8-63.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm7-32">
+<img class="lazy" data-name="sm7-32" data-card="sm7-32" data-cardname="Mudkip" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm7/sm7-32.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm12-116">
+<img class="lazy" data-name="sm12-116" data-card="sm12-116" data-cardname="Palpitoad" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm12/sm12-116.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm12-117">
+<img class="lazy" data-name="sm12-117" data-card="sm12-117" data-cardname="Seismitoad" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm12/sm12-117.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-55">
+<img class="lazy" data-name="swsh1-55" data-card="swsh1-55" data-cardname="Sobble" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh1/swsh1-55.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm10-56">
+<img class="lazy" data-name="sm10-56" data-card="sm10-56" data-cardname="Stunfisk" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm10/sm10-56.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh8-64">
+<img class="lazy" data-name="swsh8-64" data-card="swsh8-64" data-cardname="Swampert" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh8/swsh8-64.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm12-118">
+<img class="lazy" data-name="sm12-118" data-card="sm12-118" data-cardname="Throh" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm12/sm12-118.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm12-58">
+<img class="lazy" data-name="sm12-58" data-card="sm12-58" data-cardname="Tympole" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm12/sm12-58.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh2-100">
+<img class="lazy" data-name="swsh2-100" data-card="swsh2-100" data-cardname="Whiscash" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh2/swsh2-100.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh11-153">
+<img class="lazy" data-name="swsh11-153" data-card="swsh11-153" data-cardname="Arezu" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh11/swsh11-153.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh45-57">
+<img class="lazy" data-name="swsh45-57" data-card="swsh45-57" data-cardname="Ball Guy" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh45/swsh45-57.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm2-120">
+<img class="lazy" data-name="sm2-120" data-card="sm2-120" data-cardname="Brooklet Hill" data-cardtype="" data-src="https://images.pokemoncard.io/images/sm2/sm2-120.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh2-156">
+<img class="lazy" data-name="swsh2-156" data-card="swsh2-156" data-cardname="Capacious Bucket" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh2/swsh2-156.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh11-156">
+<img class="lazy" data-name="swsh11-156" data-card="swsh11-156" data-cardname="Damage Pump" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh11/swsh11-156.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-160">
+<img class="lazy" data-name="swsh1-160" data-card="swsh1-160" data-cardname="Energy Retrieval" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh1/swsh1-160.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm10-170">
+<img class="lazy" data-name="sm10-170" data-card="sm10-170" data-cardname="Energy Spinner" data-cardtype="" data-src="https://images.pokemoncard.io/images/sm10/sm10-170.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh5-125">
+<img class="lazy" data-name="swsh5-125" data-card="swsh5-125" data-cardname="Escape Rope" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh5/swsh5-125.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-163">
+<img class="lazy" data-name="swsh1-163" data-card="swsh1-163" data-cardname="Evolution Incense" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh1/swsh1-163.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh10-142">
+<img class="lazy" data-name="swsh10-142" data-card="swsh10-142" data-cardname="Gapejaw Bog" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh10/swsh10-142.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh9-141">
+<img class="lazy" data-name="swsh9-141" data-card="swsh9-141" data-cardname="Gloria" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh9/swsh9-141.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh10-146">
+<img class="lazy" data-name="swsh10-146" data-card="swsh10-146" data-cardname="Hisuian Heavy Ball" data-cardtype="" data-src="https://images.pokemoncard.io/images/swsh10/swsh10-146.png" loading="lazy" src="https://images.pokemoncard.io/images/assets/CardBack.jpg">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh10-147">
+<img class="lazy" data-name="swsh10-147" data-card="swsh10-147" data-cardname="Irida" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh10/swsh10-147.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh6-145">
+<img class="lazy" data-name="swsh6-145" data-card="swsh6-145" data-cardname="Klara" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh6/swsh6-145.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh12-159">
+<img class="lazy" data-name="swsh12-159" data-card="swsh12-159" data-cardname="Lance" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh12/swsh12-159.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh5-129">
+<img class="lazy" data-name="swsh5-129" data-card="swsh5-129" data-cardname="Level Ball" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh5/swsh5-129.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh35-56">
+<img class="lazy" data-name="swsh35-56" data-card="swsh35-56" data-cardname="Marnie" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh35/swsh35-56.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-123">
+<img class="lazy" data-name="sm1-123" data-card="sm1-123" data-cardname="Nest Ball" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-123.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh45-60">
+<img class="lazy" data-name="swsh45-60" data-card="swsh45-60" data-cardname="Professor's Research" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh45/swsh45-60.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh7-152">
+<img class="lazy" data-name="swsh7-152" data-card="swsh7-152" data-cardname="Raihan" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh7/swsh7-152.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh1-180">
+<img class="lazy" data-name="swsh1-180" data-card="swsh1-180" data-cardname="Rare Candy" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh1/swsh1-180.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh7-154">
+<img class="lazy" data-name="swsh7-154" data-card="swsh7-154" data-cardname="Rescue Carrier" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh7/swsh7-154.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh8-239">
+<img class="lazy" data-name="swsh8-239" data-card="swsh8-239" data-cardname="Schoolgirl" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh8/swsh8-239.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh8-240">
+<img class="lazy" data-name="swsh8-240" data-card="swsh8-240" data-cardname="Shauna" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh8/swsh8-240.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh2-167">
+<img class="lazy" data-name="swsh2-167" data-card="swsh2-167" data-cardname="Sonia" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh2/swsh2-167.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm11-208">
+<img class="lazy" data-name="sm11-208" data-card="sm11-208" data-cardname="Stadium Nav" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm11/sm11-208.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-132">
+<img class="lazy" data-name="sm1-132" data-card="sm1-132" data-cardname="Switch" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-132.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-134">
+<img class="lazy" data-name="sm1-134" data-card="sm1-134" data-cardname="Timer Ball" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-134.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh9-150">
+<img class="lazy" data-name="swsh9-150" data-card="swsh9-150" data-cardname="Ultra Ball" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh9/swsh9-150.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=swsh2-171">
+<img class="lazy" data-name="swsh2-171" data-card="swsh2-171" data-cardname="Capture Energy" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/swsh2/swsh2-171.png" style="" data-original-title="" title="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-169">
+<img class="lazy" data-name="sm1-169" data-card="sm1-169" data-cardname="Fighting Energy" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-169.png" style="">
+<span class="card-count">x7</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-137">
+<img class="lazy" data-name="sm1-137" data-card="sm1-137" data-cardname="Rainbow Energy" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-137.png" style="">
+<span class="card-count">x1</span>
+</a></div><div class="m-2 d-inline-block"><a class="ygodeckcard" href="/card/?search=sm1-166">
+<img class="lazy" data-name="sm1-166" data-card="sm1-166" data-cardname="Water Energy" data-cardtype="" loading="lazy" src="https://images.pokemoncard.io/images/sm1/sm1-166.png" style="">
+<span class="card-count">x5</span>
+</a></div></div>
