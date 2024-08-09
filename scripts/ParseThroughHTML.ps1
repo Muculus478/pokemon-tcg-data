64d2d69c99bfca5f29017add32a1dae79932846f
Muculus478 <esevart@gmail.com>
2024-08-09 16:01:23 -0400

    Updated HTML Raw Body Data

--- scripts/ParseThroughHTML.ps1
+++ scripts/ParseThroughHTML.ps1
@@ -2,6 +2,9 @@
 $fileContent = Get-Content -Path $filePath
 Write-Host "Original Content:"
 Write-Host $fileContent
-$filteredContent = $fileContent | Where-Object { $_ -match "var maindeckjs" }
+$filteredContent1 = $fileContent | Where-Object { $_ -match "var maindeckjs" }
+$filteredContent2 = $fileContent | Where-Object { $_ -match "var maindeckcount" }
 Write-Host "`nFiltered Content (lines containing 'var maindeckjs'):"
-Write-Host $filteredContent
+Write-Host "`nFiltered Content (lines containing 'var maindeckcount'):"
+Write-Host $filteredContent1
+Write-Host $filteredContent2
