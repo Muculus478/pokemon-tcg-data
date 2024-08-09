fa306f65d06798700f8250678c4c5cab92531b93
Muculus478 <esevart@gmail.com>
2024-08-09 15:50:51 -0400

    Updates

---
+++ scripts/ParseThroughHTML.ps1
@@ -0,0 +1,7 @@
+﻿$filePath = "C:\Users\Gunna\Documents\GitHub\pokemon-tcg-data\scripts\MuddyWaters.txt"
+$fileContent = Get-Content -Path $filePath
+Write-Host "Original Content:"
+Write-Host $fileContent
+$filteredContent = $fileContent | Where-Object { $_ -match "var maindeckjs" }
+Write-Host "`nFiltered Content (lines containing 'var maindeckjs'):"
+Write-Host $filteredContent
