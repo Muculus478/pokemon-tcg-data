6c396b426b55c3f0994a5e5c0bf35d8134e0ec62
Muculus478 <esevart@gmail.com>
2024-08-09 16:33:07 -0400

    Attempting to Output Image Data and Card Data

--- scripts/ParseThroughHTML.ps1
+++ scripts/ParseThroughHTML.ps1
@@ -3,8 +3,21 @@ $fileContent = Get-Content -Path $filePath
 Write-Host "Original Content:"
 Write-Host $fileContent
 $filteredContent1 = $fileContent | Where-Object { $_ -match "var maindeckjs" }
-$filteredContent2 = $fileContent | Where-Object { $_ -match "var maindeckcount" }
+
+$fileContentRaw = Get-Content -Path $filePath -Raw
+$pattern = '(?s)(?<=<form><textarea\b[^>]*>)(.*?)(?=</textarea></form>)'
+$matches = [regex]::Matches($fileContentRaw, $pattern)
+foreach ($match in $matches) {
+    Write-Host "Captured content:"
+    Write-Host $match.Value
+}
+
 Write-Host "`nFiltered Content (lines containing 'var maindeckjs'):"
-Write-Host "`nFiltered Content (lines containing 'var maindeckcount'):"
 Write-Host $filteredContent1
-Write-Host $filteredContent2
+
+$outputFilePath = "C:\Users\Gunna\Documents\GitHub\pokemon-tcg-data\scripts\output.txt"
+Clear-Content -Path $outputFilePath -ErrorAction SilentlyContinue
+foreach ($match in $matches) {
+    $match.Value | Out-File -FilePath $outputFilePath -Append
+}
+Write-Host "Captured content"
