678cae64226b26fa1ab0a6a9f928c758a0fbf5a2
Muculus478 <esevart@gmail.com>
2024-08-18 19:01:30 -0400

    deleted bad scripts

--- scripts/ImportSelenium.ps1
+++
@@ -1,6 +0,0 @@
-OK so the file path for Selenium Java Download is here. C:\Users\Gunna\Downloads\selenium-java-4.23.0   
-please create powershell script to use selenium to get RAW contents of this website. https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660. Here
- is my Java file path"C:\Program Files\Java\jre-1.8\bin\java.exe"  And here is the path where JDK lives OK so the file path for 
-Selenium Java Download is here. C:\Users\Gunna\Downloads\selenium-java-4.23.0   please create powershell script to use 
-selenium to get RAW contents of this website. https://pokemoncard.io/deck/muddy-waters-sglc-semi-gym-leader-challenge-35660. 
-Here is my Java file path"C:\Program Files\Java\jre-1.8\bin\java.exe"
